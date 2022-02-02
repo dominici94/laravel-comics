c9df65bf8c0ddf26eb29aba1d6a2b6339d0c3e85
dominici94 <mattiadominici94@gmail.com>
2022-02-02 20:00:08 +0100

    header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,116 @@
 <footer>
-    <div class="container">
-        made with love by a booleaner
+    <div class="top-footer">
+        <div class="container">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <div class="img-container">
+                                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="esempio">
+                            </div>
+                            digital comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="img-container">
+                                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="esempio">
+                            </div>
+                            dc merchandise
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="img-container">
+                                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="esempio">
+                            </div>
+                            subscription
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="img-container">
+                                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="esempio">
+                            </div>
+                            comic shop locator
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="img-container">
+                                <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="esempio">
+                            </div>
+                            dc power visa
+                        </a>
+                    </li>
+                </ul>
+        </div>
+        
+    </div>
+    <div class="mid-footer">
+        <div class="container">
+            <div class="mid-container left">
+                    <div class="column">
+                        <h2>dc comics</h2>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h2>shop</h2>
+                        <ul>
+                            <li><a href="#">Shop dc</a></li>
+                            <li><a href="#">Shop dc collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h2>dc</h2>
+                        <ul>
+                            <li><a href="#">Terms of use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">jobs</a></li>
+                            <li><a href="#">subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CSPC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h2>sites</h2>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+            </div>
+            <div class="mid-container right">
+            </div>
+        </div>
+    </div>
+    <div class="bottom-footer">
+        <div class="container">
+            <div class="left-btm-container">
+                <button>sign-up now!</button>
+            </div>
+            <div class="right-btm-container">
+                <a href="#">Follow us</a>
+                <img src="{{asset('img/footer-facebook.png')}}" alt="">
+                <img src="{{asset('img/footer-twitter.png')}}" alt="">
+                <img src="{{asset('img/footer-youtube.png')}}" alt="">
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+                <img src="{{asset('img/footer-periscope.png')}}" alt="">
+                
+            </div>
+        </div>
     </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,26 @@
 <header>
-    <nav>
-        <ul>
-            <li>
-                <a href="/">Home page</a>
-            </li>
-            <li>
-                <a href="/chi-siamo">Chi siamo</a>
-            </li>
+    <div class="container-header">
+        <div class="logo">
+            <img src="../img/dc-logo.png" alt="logo dc">
+        </div>
+        
+        <nav>
+            {{-- <ul>
+                <li v-for="(link,index) in links" :key="index"><a :class="{active: link.current}" :href="link.url">{{link.text}}</a></li>
+            </ul> --}}
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#" class="active">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
 
-            <li>
-                <a href="/contatti">Contatti</a>
-            </li>
-        </ul>
-    </nav>
-    <img src="{{asset('img/dc-logo.png')}}" alt="logo">
+            </ul>
+        </nav>
+  </div>
 </header>
